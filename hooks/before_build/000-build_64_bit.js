82e6212576cb44a5f534c18e1fc87cacda8ef319
Fernando Virdia <32302615+fvirdia@users.noreply.github.com>
2021-12-11 00:24:47 +0000

    Update 000-build_64_bit.js

--- hooks/before_build/000-build_64_bit.js
+++ hooks/before_build/000-build_64_bit.js
@@ -3,7 +3,7 @@
 module.exports = function(context) {
 
     /** @external */
-    var deferral = requrie('q').defer(),
+    var deferral = require('q').defer(),
         UpdateConfig = require('./../update_config.js'),
         updateConfig = new UpdateConfig(context);
 
